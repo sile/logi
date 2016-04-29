839bfd216a71a94cff9d469b2f230d001a3333ff
Takeru Ohta <phjgt308@gmail.com>
2016-04-29 11:51:35 +0900

    mv logi_sink_{noop,standalone_writer_proc}.erl

---
+++ src/logi_sink_standalone_writer_proc.erl
@@ -0,0 +1,54 @@
+%% @copyright 2014-2016 Takeru Ohta <phjgt308@gmail.com>
+%%
+%% @doc A noop sink process which sends the passed writer to the parent
+%% @private
+%% @end
+-module(logi_sink_standalone_writer_proc).
+
+-behaviour(gen_server).
+
+%%----------------------------------------------------------------------------------------------------------------------
+%% Exported API
+%%----------------------------------------------------------------------------------------------------------------------
+-export([start_link/1]).
+
+%%----------------------------------------------------------------------------------------------------------------------
+%% 'gen_server' Callback API
+%%----------------------------------------------------------------------------------------------------------------------
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+%%----------------------------------------------------------------------------------------------------------------------
+%% Exported Functions
+%%----------------------------------------------------------------------------------------------------------------------
+%% @doc Starts a process
+-spec start_link(logi_sink_writer:writer()) -> {ok, pid()} | {error, Reason::term()}.
+start_link(Writer) ->
+    gen_server:start_link(?MODULE, Writer, []).
+
+%%----------------------------------------------------------------------------------------------------------------------
+%% 'gen_server' Callback Functions
+%%----------------------------------------------------------------------------------------------------------------------
+%% @private
+init(Writer) ->
+    ok = logi_sink_proc:send_writer(Writer),
+    {ok, []}.
+
+%% @private
+handle_call(_Request, _From, State) ->
+    {noreply, State}.
+
+%% @private
+handle_cast(_Request, State) ->
+    {noreply, State}.
+
+%% @private
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%% @private
+terminate(_Reason, _State) ->
+    ok.
+
+%% @private
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
