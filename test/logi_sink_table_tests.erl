f317a229965a69dda11cdee70707390a4c874b4e
Takeru Ohta <phjgt308@gmail.com>
2016-05-07 16:29:03 +0900

    Unification of terminology (s/sink/sink writer/)

--- test/logi_sink_table_tests.erl
+++ test/logi_sink_table_tests.erl
@@ -1,4 +1,5 @@
-%% @copyright 2014-2015 Takeru Ohta <phjgt308@gmail.com>
+%% @copyright 2014-2016 Takeru Ohta <phjgt308@gmail.com>
+%% @end
 -module(logi_sink_table_tests).
 
 -include_lib("eunit/include/eunit.hrl").
@@ -7,8 +8,8 @@
 %% Macros
 %%----------------------------------------------------------------------------------------------------------------------
 -define(CHANNEL, logi_test_log).
--define(TEST_SINK, logi_builtin_sink_fun).
--define(SINK(Id), logi_sink_writer:new(?TEST_SINK, Id)). % XXX:
+-define(WRITER(), ?WRITER(fun (_, _, _) -> [] end)).
+-define(WRITER(Fun), logi_sink_writer:new(logi_builtin_sink_fun, Fun)).
 
 %%----------------------------------------------------------------------------------------------------------------------
 %% Unit Tests
@@ -34,21 +35,21 @@ register_test_() ->
      fun ()  -> logi_sink_table:new(?CHANNEL) end,
      fun (T) -> ok = logi_sink_table:delete(T) end,
      [
-      {"Registers a sink",
+      {"Registers a sink writer",
        fun () ->
-               ?assertEqual(ok, logi_sink_table:register(?CHANNEL, ?TEST_SINK, ?SINK(?TEST_SINK), debug, [])),
-               ?assertEqual([?TEST_SINK], logi_sink_table:which_sinks(?CHANNEL))
+               ?assertEqual(ok, logi_sink_table:register(?CHANNEL, foo, ?WRITER(), debug, [])),
+               ?assertEqual([foo], logi_sink_table:which_sinks(?CHANNEL))
        end},
-      {"Deregisters a sink",
+      {"Deregisters a sink writer",
        fun () ->
-               ok = logi_sink_table:register(?CHANNEL, hoge, ?SINK(?TEST_SINK), debug, []),
-               ok = logi_sink_table:register(?CHANNEL, fuga, ?SINK(?TEST_SINK), debug, []),
-               ?assertEqual(lists:sort([hoge, fuga]), lists:sort(logi_sink_table:which_sinks(?CHANNEL))),
+               ok = logi_sink_table:register(?CHANNEL, foo, ?WRITER(), debug, []),
+               ok = logi_sink_table:register(?CHANNEL, bar, ?WRITER(), debug, []),
+               ?assertEqual(lists:sort([foo, bar]), lists:sort(logi_sink_table:which_sinks(?CHANNEL))),
 
-               ?assertEqual(ok, logi_sink_table:deregister(?CHANNEL, hoge, debug)),
-               ?assertEqual([fuga], logi_sink_table:which_sinks(?CHANNEL)),
+               ?assertEqual(ok, logi_sink_table:deregister(?CHANNEL, foo, debug)),
+               ?assertEqual([bar], logi_sink_table:which_sinks(?CHANNEL)),
 
-               ?assertEqual(ok, logi_sink_table:deregister(?CHANNEL, fuga, debug)),
+               ?assertEqual(ok, logi_sink_table:deregister(?CHANNEL, bar, debug)),
                ?assertEqual([], logi_sink_table:which_sinks(?CHANNEL))
        end}
      ]}.
@@ -58,26 +59,32 @@ select_test_() ->
      fun ()  -> logi_sink_table:new(?CHANNEL) end,
      fun (T) -> ok = logi_sink_table:delete(T) end,
      [
-      {"Selects sinks that meet the condition",
+      {"Selects sink writers that meet the condition",
        fun () ->
-               Sink = fun (Id, Condition) -> {Id, Condition, ?SINK(Id)} end,
-               Sink1 = Sink(s1, debug),
-               Sink2 = Sink(s2, {info, alert}),
-               Sink3 = Sink(s3, [info]),
-               Sink4 = Sink(s4, #{severity => info, application => stdlib}),
-               Sink5 = Sink(s5, #{severity => info, module => lists}),
+               Writer =
+                   fun (Id, Condition) ->
+                           Fun = fun (_, _, _) -> atom_to_list(Id) end,
+                           {{Id, Condition, ?WRITER(Fun)}, Fun}
+                   end,
+               {Writer1, V1} = Writer(w1, debug),
+               {Writer2, V2} = Writer(w2, {info, alert}),
+               {Writer3, V3} = Writer(w3, [info]),
+               {Writer4, V4} = Writer(w4, #{severity => info, application => stdlib}),
+               {Writer5, V5} = Writer(w5, #{severity => info, module => lists}),
 
                lists:foreach(fun ({Id, C, S}) -> logi_sink_table:register(?CHANNEL, Id, S, C, []) end,
-                             [Sink1, Sink2, Sink3, Sink4, Sink5]),
+                             [Writer1, Writer2, Writer3, Writer4, Writer5]),
 
                Select =
                    fun (Severity, Application, Module) ->
-                           lists:sort([Id || {_, Id} <- logi_sink_table:select(?CHANNEL, Severity, Application, Module)])
+                           lists:sort(
+                             [logi_sink_writer:get_state(W) ||
+                                 W <- logi_sink_table:select(?CHANNEL, Severity, Application, Module)])
                    end,
 
-               ?assertEqual([s1],                 Select(debug, stdlib, lists)),
-               ?assertEqual([s1, s2, s3, s4, s5], Select(info, stdlib, lists)),
-               ?assertEqual([s1, s2, s4],         Select(notice, stdlib, dict))
+               ?assertEqual([V1],                 Select(debug, stdlib, lists)),
+               ?assertEqual([V1, V2, V3, V4, V5], Select(info, stdlib, lists)),
+               ?assertEqual([V1, V2, V4],         Select(notice, stdlib, dict))
        end},
       {"If an unknown table is specified, `select/4` will return an empty list",
        fun () ->
@@ -93,8 +100,8 @@ select_test_() ->
                                 lists:foreach(
                                   fun (_) ->
                                           timer:sleep(1),
-                                          Sinks = logi_sink_table:select(?CHANNEL, info, stdlib, lists),
-                                          ?assert(Sinks =:= [] orelse length(Sinks) =:= 1)
+                                          Writers = logi_sink_table:select(?CHANNEL, info, stdlib, lists),
+                                          ?assert(Writers =:= [] orelse length(Writers) =:= 1)
                                   end,
                                   lists:seq(1, 1000))
                         end)) || _ <- lists:seq(1, 100)],
@@ -104,9 +111,9 @@ select_test_() ->
                            receive
                                {'DOWN', Monitor, _, _, _} -> Recur(lists:delete(Monitor, List))
                            after 0 ->
-                                   Sink = ?SINK(?TEST_SINK),
-                                   ok = logi_sink_table:register(?CHANNEL, hoge, Sink, debug, []),
-                                   logi_sink_table:deregister(?CHANNEL, hoge, debug),
+                                   Writer = ?WRITER(),
+                                   ok = logi_sink_table:register(?CHANNEL, foo, Writer, debug, []),
+                                   logi_sink_table:deregister(?CHANNEL, foo, debug),
                                    Recur(List)
                            end
                    end,
