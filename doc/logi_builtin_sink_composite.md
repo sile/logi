71da31461c7ebc03ffb4f7f5986b2a233bc785a9
Takeru Ohta <phjgt308@gmail.com>
2016-05-07 19:27:10 +0900

    Integrate logi_builtin_sink_composite_writer into logi_builtin_sink_composite

--- doc/logi_builtin_sink_composite.md
+++ doc/logi_builtin_sink_composite.md
@@ -9,6 +9,8 @@ A built-in composite sink.
 
 Copyright (c) 2014-2016 Takeru Ohta <phjgt308@gmail.com>
 
+__Behaviours:__ [`gen_server`](gen_server.md).
+
 <a name="description"></a>
 
 ## Description ##
@@ -22,12 +24,34 @@ This module is provided for debuging/testing purposes only.
 ## Function Index ##
 
 
-<table width="100%" border="1" cellspacing="0" cellpadding="2" summary="function index"><tr><td valign="top"><a href="#new-2">new/2</a></td><td>Creats a new sink instance.</td></tr></table>
+<table width="100%" border="1" cellspacing="0" cellpadding="2" summary="function index"><tr><td valign="top"><a href="#get_active_writer-1">get_active_writer/1</a></td><td>Gets the active child writer.</td></tr><tr><td valign="top"><a href="#get_children-1">get_children/1</a></td><td>Returns a list of children.</td></tr><tr><td valign="top"><a href="#new-2">new/2</a></td><td>Creats a new sink instance.</td></tr><tr><td valign="top"><a href="#set_active_writer-2">set_active_writer/2</a></td><td>Sets the <code>Nth</code> cihld to be active.</td></tr></table>
 
 
 <a name="functions"></a>
 
 ## Function Details ##
+
+<a name="get_active_writer-1"></a>
+
+### get_active_writer/1 ###
+
+<pre><code>
+get_active_writer(Pid::pid()) -&gt; <a href="logi_sink_writer.md#type-writer">logi_sink_writer:writer()</a> | undefined
+</code></pre>
+<br />
+
+Gets the active child writer
+
+<a name="get_children-1"></a>
+
+### get_children/1 ###
+
+<pre><code>
+get_children(Pid::pid()) -&gt; [<a href="logi_sink.md#type-sink">logi_sink:sink()</a>]
+</code></pre>
+<br />
+
+Returns a list of children
 
 <a name="new-2"></a>
 
@@ -39,4 +63,15 @@ new(Id::<a href="logi_sink.md#type-id">logi_sink:id()</a>, Children::[<a href="l
 <br />
 
 Creats a new sink instance
+
+<a name="set_active_writer-2"></a>
+
+### set_active_writer/2 ###
+
+<pre><code>
+set_active_writer(Pid::pid(), Nth::pos_integer()) -&gt; ok
+</code></pre>
+<br />
+
+Sets the `Nth` cihld to be active
 
--- src/logi_builtin_sink_composite.erl
+++ src/logi_builtin_sink_composite.erl
@@ -8,10 +8,43 @@
 %% @end
 -module(logi_builtin_sink_composite).
 
+-behaviour(gen_server).
+
 %%----------------------------------------------------------------------------------------------------------------------
 %% Exported API
 %%----------------------------------------------------------------------------------------------------------------------
 -export([new/2]).
+
+-export([get_children/1]).
+-export([get_active_writer/1]).
+-export([set_active_writer/2]).
+
+%%----------------------------------------------------------------------------------------------------------------------
+%% Application Internal API
+%%----------------------------------------------------------------------------------------------------------------------
+-export([start_link/1]).
+
+%%----------------------------------------------------------------------------------------------------------------------
+%% 'gen_server' Callback API
+%%----------------------------------------------------------------------------------------------------------------------
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+%%----------------------------------------------------------------------------------------------------------------------
+%% Macros & Records & Types
+%%----------------------------------------------------------------------------------------------------------------------
+-record(child,
+        {
+          sink   :: logi_sink:sink(),
+          sup    :: logi_sink_proc:sink_sup(),
+          writer :: logi_sink_writer:writer() | undefined
+        }).
+
+-define(STATE, ?MODULE).
+-record(?STATE,
+        {
+          children = [] :: [#child{}],
+          active        :: logi_sink_writer:writer() | undefined
+        }).
 
 %%----------------------------------------------------------------------------------------------------------------------
 %% Exported Functions
@@ -20,4 +53,92 @@
 -spec new(logi_sink:id(), [logi_sink:sink()]) -> logi_sink:sink().
 new(Id, Children) ->
     _ = is_list(Children) andalso lists:all(fun logi_sink:is_sink/1, Children) orelse error(badarg, [Id, Children]),
-    logi_sink:new(#{id => Id, start => {logi_builtin_sink_composite_writer, start_link, [Children]}}).
+    logi_sink:new(#{id => Id, start => {?MODULE, start_link, [Children]}}).
+
+%% @doc Returns a list of children
+-spec get_children(pid()) -> [logi_sink:sink()].
+get_children(Pid) ->
+    gen_server:call(Pid, get_children).
+
+%% @doc Gets the active child writer
+-spec get_active_writer(pid()) -> logi_sink_writer:writer() | undefined.
+get_active_writer(Pid) ->
+    gen_server:call(Pid, get_active_writer).
+
+%% @doc Sets the `Nth' cihld to be active
+-spec set_active_writer(pid(), pos_integer()) -> ok.
+set_active_writer(Pid, Nth) ->
+    gen_server:cast(Pid, {set_active_writer, Nth}).
+
+%%----------------------------------------------------------------------------------------------------------------------
+%% Application Internal Functions
+%%----------------------------------------------------------------------------------------------------------------------
+%% @private
+-spec start_link([logi_sink:sink()]) -> {ok, pid()} | {error, Reason::term()}.
+start_link(Children = [_|_]) ->
+    gen_server:start_link(?MODULE, Children, []).
+
+%%----------------------------------------------------------------------------------------------------------------------
+%% 'gen_server' Callback Functions
+%%----------------------------------------------------------------------------------------------------------------------
+%% @private
+init(Sinks) ->
+    Start =
+        fun (_, {error, Reason}) ->
+                {error, Reason};
+            (Sink, {ok, Acc}) ->
+                case logi_sink_proc:start_child(Sink) of
+                    {error, Reason} -> {error, Reason};
+                    {ok, SinkSup}   ->
+                        true = link(SinkSup),
+                        Writer = logi_sink_proc:recv_writer_from_child(SinkSup, 100),
+                        Child = #child{sink = Sink, sup = SinkSup, writer = Writer},
+                        {ok, [Child | Acc]}
+                end
+        end,
+    case lists:foldl(Start, {ok, []}, Sinks) of
+        {error, Reason} -> {stop, Reason};
+        {ok, Children0} ->
+            Children1 = [Active | _] = lists:reverse(Children0),
+            State =
+                #?STATE{
+                    children = Children1,
+                    active   = Active#child.writer
+                   },
+            ok = logi_sink_proc:send_writer_to_parent(State#?STATE.active),
+            {ok, State}
+    end.
+
+%% @private
+handle_call(get_children, _From, State) ->
+    {reply, [C#child.sink || C <- State#?STATE.children], State};
+handle_call(get_active_writer, _From, State) ->
+    {reply, State#?STATE.active, State};
+handle_call(_Request, _From, State) ->
+    {noreply, State}.
+
+%% @private
+handle_cast({set_active_writer, Nth}, State) ->
+    Active = (lists:nth(Nth, State#?STATE.children))#child.writer,
+    ok = logi_sink_proc:send_writer_to_parent(Active),
+    {noreply, State#?STATE{active = Active}};
+handle_cast(_Request, State) ->
+    {noreply, State}.
+
+%% @private
+handle_info({sink_writer, SinkSup, Writer}, State) ->
+    Child0 = lists:keyfind(SinkSup, #child.sup, State#?STATE.children),
+    Child1 = Child0#child{writer = Writer},
+    Children = lists:keystore(SinkSup, #child.sup, State#?STATE.children, Child1),
+    _ = Child0#child.writer =:= State#?STATE.active andalso logi_sink_proc:send_writer_to_parent(Child1#child.writer),
+    {noreply, State#?STATE{children = Children}};
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%% @private
+terminate(_Reason, _State) ->
+    ok.
+
+%% @private
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
