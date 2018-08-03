9d012b47f72f3a3709b8e9f640be2c08414401e9
Takeru Ohta <phjgt308@gmail.com>
2018-08-03 15:11:25 +0900

    Use `OTP_RELEASE` macro for detecting the existence of the new try/catch syntax

--- src/logi_logger.erl
+++ src/logi_logger.erl
@@ -56,12 +56,14 @@
 -opaque logger() :: #?LOGGER{}.
 %% A logger instance
 
--ifdef('FUN_STACKTRACE').
--define(CAPTURE_STACKTRACE, ).
--define(GET_STACKTRACE, erlang:get_stacktrace()).
--else.
+-ifdef('OTP_RELEASE').
+%% The 'OTP_RELEASE' macro introduced at OTP-21,
+%% so we can use it for detecting whether the Erlang compiler supports new try/catch syntax or not.
 -define(CAPTURE_STACKTRACE, :__StackTrace).
 -define(GET_STACKTRACE, __StackTrace).
+-else.
+-define(CAPTURE_STACKTRACE, ).
+-define(GET_STACKTRACE, erlang:get_stacktrace()).
 -endif.
 
 %%----------------------------------------------------------------------------------------------------------------------
